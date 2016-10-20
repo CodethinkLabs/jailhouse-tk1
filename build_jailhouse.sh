03ee0ac42ca671a1ba21402548636e851f1ab442
Christopher Phang <christopher.phang@codethink.co.uk>
2016-10-20 14:31:38 +0100

    Checkout only specific commit of jailhouse
    With current kernel configs for 4.4.23, jailhouse build fails on recent
    commits due to CONFIG_ARM_GIC_V2 being required
    (commit a5d1415f3cb2706731eb28b26e5e4f835ed87858) was identified through
    git bisect.
    
    We revert therefore to a known good commit.

--- build_jailhouse.sh
+++ build_jailhouse.sh
@@ -16,6 +16,8 @@ git clone https://github.com/siemens/jailhouse.git
 
 #Copy TK1 specific files over and make and install jailhouse
 cd jailhouse
+git checkout b267f36bfd9583425a7d151c977c4f5d602421e3
+
 cp $VAGRANTDIR/jailhouse_config/jetson-tk1.c configs/jetson-tk1.c
 cp $VAGRANTDIR/jailhouse_config/config.h hypervisor/include/jailhouse/config.h
 make ARCH=arm CROSS_COMPILE=/usr/bin/arm-linux-gnueabihf- KDIR=$KDIR
