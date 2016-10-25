cd9e688331ce78416d09237ccb12846d54d39c81
Christopher Phang <christopher.phang@codethink.co.uk>
2016-10-25 16:55:00 +0100

    Adding dependencies and linkages required for openstack building
    The nouveau stack consists of the drm driver (to be added in a later
    commit), libdrm and mesa. Kmscube is a utility then used to test
    this stack.
    
    Mesa in particular has a large set of dependencies that need to be met.
    These scripts (to be sourced) are to meet these dependencies

---
+++ graphics_openstack/nouveau-env.sh
@@ -0,0 +1,33 @@
+
+DEV_HOME=/home/vagrant
+NVD=/home/vagrant/install       
+LD_LIBRARY_PATH=$NVD/lib
+PKG_CONFIG_PATH=$NVD/lib/pkgconfig/:$NVD/share/pkgconfig/
+ACLOCAL="aclocal -I $NVD/share/aclocal"
+
+DRM_SETTINGS="--disable-cairo-tests \
+--disable-intel \
+--disable-radeon \
+--disable-vmwgfx \
+--disable-freedreno \
+--disable-valgrind \
+--disable-amdgpu \
+--enable-static"
+
+MESA_SETTINGS="--without-dri-drivers \
+--with-gallium-drivers=nouveau \
+--enable-gbm \
+--enable-egl \
+--with-egl-platforms=drm,x11,wayland \
+--enable-gles1 \
+--enable-gles2 \
+--enable-opengl \
+--enable-osmesa \
+--enable-shared-glapi \
+--enable-dri3 \
+--enable-glx \
+--enable-glx-tls"
+
+HOST=arm-linux-gnueabihf
+
+export DEV_HOME NVD LD_LIBRARY_PATH PKG_CONFIG_PATH ACLOCAL HOST DRM_SETTINGS MESA_SETTINGS
