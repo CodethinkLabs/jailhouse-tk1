d45eca19f0b5be5aefa4d5f7910ad5bbf3a1066d
Christopher Phang <christopher.phang@codethink.co.uk>
2016-10-31 12:14:09 +0000

    Removal of previous graphics_openstack code for tegra_script code
    -Vagrantfile is streamlined, removing code in favour of existing scripts
     (cross_compile_setup.sh) and putting openstack building deps in separate
     file.
    -Vagrantfile directly calls scripts from tegra_rootfs_scripts repo.
    -Copying rootfs is done in exactly the same fashion as in setup_rootfs repo.

---
+++ graphics_openstack/get_graphics_stack_packages.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#We need to use wayland scanner from native libwayland-dev (from ubuntu disto) but the armhf libaries from libwayland-dev:armhf
+wget http://launchpadlibrarian.net/188497486/libwayland-dev_1.6.0-2_amd64.deb
+mkdir libwayland-dev_amd64
+dpkg-deb -x libwayland-dev_1.6.0-2_amd64.deb libwayland-dev_amd64/
+cp libwayland-dev_amd64/usr/bin/wayland-scanner /usr/bin/
+
+#Clone the relevant libraries
+git clone git://anongit.freedesktop.org/xcb/pthread-stubs
+git clone git://anongit.freedesktop.org/mesa/drm
+git clone --depth 1 --branch mesa-12.0.2 git://anongit.freedesktop.org/mesa/mesa
+git clone --branch "gk20a" https://github.com/Gnurou/kmscube.git
+
---
+++ graphics_openstack/script_deps.sh
@@ -0,0 +1,18 @@
+SCRIPT_DEPS="git 
+proot 
+binfmt-support
+qemu
+qemu-user-static
+autoconf 
+automake 
+libtool 
+pkg-config 
+bison 
+flex 
+bc 
+glib-2.0 
+xutils-dev 
+python-mako 
+intltool"
+
+export SCRIPT_DEPS
