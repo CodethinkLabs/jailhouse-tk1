40c0877c05d807ebb0dc97c7dba32f4d846ea10c
Christopher Phang <christopher.phang@codethink.co.uk>
2016-10-31 16:10:24 +0000

    Integration of graphics_openstack rootfs to create_linux_sys script
    Replace L4T custom binaries and FS with one augmented with opensource
    graphics stack.
    
    In addition, to reduce duplication between scripts we use a
    deploy_config file to share variables between the two files.
    
    Also remove from create_linux_sys.sh copying over of old kernel files

--- create_linux_sys.sh
+++ create_linux_sys.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
-#Creates L4T (currently R21.5.0) to roofs directory on L4T folder
+#Extracts L4T FS (currently R21.5.0) to rootfs directory.
 #
-#Makes a L4T system from fs and kernel, and applies tegra specific
-#binaries as well.
 
-L4T_FS_ARCHIVE="l4t-Jetson-TK1-Sample-Root-Filesystem-R21-5.tbz2"
-L4T_LINUX_ARCHIVE="Tegra124_Linux_R21.5.0_armhf.tbz2"
-
+ROOTFS="rootfs"
 SDCARD=$1
 
 if [ "$EUID" -ne 0 ]
@@ -14,44 +10,31 @@ then echo "Please run as root"
 	exit 1
 fi
 
-echo "Extracting L4T Linux Archive"
-if [ !  -f "$L4T_LINUX_ARCHIVE" ]
-then wget "http://developer.download.nvidia.com/embedded/L4T/r21_Release_v5.0/Tegra124_Linux_R21.5.0_armhf.tbz2"
-fi
- 
-tar xpf $L4T_LINUX_ARCHIVE
-if [ $? -ne 0 ]
-then echo "Extraction of L4T linux Archive failed"
-    exit 1
-fi
-
-cd Linux_for_Tegra
-
-mount $SDCARD rootfs
+mount $SDCARD $ROOTFS
 if [ $? -ne 0 ]
 then echo "Mounting of $SDCARD failed"
     exit 1
 fi
 
+source deploy_config
 echo "Extracting L4T FS Archive"
-if [ ! -f "../$L4T_FS_ARCHIVE" ]
-then wget "http://developer.nvidia.com/embedded/dlc/l4t-Jetson-TK1-Sample-Root-Filesystem-R21-5"
+if [ ! -f "../$PREPARED_ROOTFS_NAME" ]
+then echo "L4T FS not present!"
+    exit 1
 fi
 
-cd rootfs
-tar xpf ../../$L4T_FS_ARCHIVE
+cd $ROOTFS
+
+tar xpf ../../$PREPARED_ROOTFS_NAME
 if [ $? -ne 0 ]
 then echo "Extraction of L4T FS archive failed"
     cd ..
-    umount rootfs
+    umount $ROOTFS
     exit 1
 fi
 cd ..
 
-echo "Applying binaries"
-./apply_binaries.sh
-
 sync
-umount rootfs
+umount $ROOTFS
 echo "SD card ready"
 
